cb1e5b20817500504652551267b9a88681b2c145
Shashwat Pandey <pandeyshashwatptu@gmail.com>
2024-04-24 05:33:56 +0530

    Feat: Added routing structure for version api for problems

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const { PORT } = require('./config/server.config');
+const apiRouter = require('./routes');
 
-
-const {PORT} = require('./config/server.config');
 
 const app = express();
+
 app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
-app.get('/ping',(req,res)=>{
-    return res.json({message:'Problem Service is alive'});
+
+// If any request comes and route starts with /api, we map it to apiRouter
+app.use('/api', apiRouter);
+
+
+app.get('/ping', (req, res) => {
+    return res.json({message: 'Problem Service is alive'});
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server Started at PORT : ${PORT}`);
+
+app.listen(PORT, () => {
+    console.log(`Server started at PORT: ${PORT}`);
 });
